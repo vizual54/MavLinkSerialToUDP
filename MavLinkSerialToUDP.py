d43136e9a4f5b7b0ae745cfee4a9283c3eb52d7d
Vizual54 <nils.hogberg@gmail.com>
2014-10-24 10:50:48 +0000

    Fixed mav message from serial

--- MavLinkSerialToUDP.py
+++ MavLinkSerialToUDP.py
@@ -29,7 +29,7 @@ if ( len(sys.argv) >= 5 ):
 else:
     udp_port = "14550"
 
-# Create a sensor log with date and time
+# Create log
 layout = '%(asctime)s - %(levelname)s - %(message)s'
 logging.basicConfig(filename='/tmp/MavLinkSerialToUDP.log', level=logging.INFO, format=layout)
 
@@ -43,56 +43,68 @@ py_serial    = None
 connection = False
 
 def open_port(portname, baudrate):
-  try:
-    # Set timeouts to 4 ms. 3.5 ms is the measured bias when long msg get cut!
-    return serial.Serial(portname, baudrate, timeout=0.004, writeTimeout=0.004)
-  except serial.SerialException as e:
-    logging.debug("Could not open serial port: {}".format(portname, e))
-    print ("Could not open serial port: {}".format(portname, e))
-  return None
+    try:
+        #return serial.Serial(portname, baudrate, timeout=0.004, writeTimeout=0.004)
+        return serial.Serial(portname, baudrate)
+    except serial.SerialException as e:
+        logging.debug("Could not open serial port: {}".format(portname, e))
+        print ("Could not open serial port: {}".format(portname, e))
+    return None
   
 def ser_write(output):
     try:
-      bytes = py_serial.write(output)
+        bytes = py_serial.write(output)
     except serial.SerialTimeoutException as e:
-      logging.error("Write timeout on serial port")
+        logging.error("Write timeout on serial port")
     except serial.SerialException as e:
-      logging.error("Write exception serial port")
+        logging.error("Write exception serial port")
     finally:
-      py_serial.flushInput()                                             # Workaround: free write buffer (otherwise the Arduino board hangs)
-	
+        py_serial.flushInput()
+
 def udp_write(msg):
     bytes = udp_socket.sendto(msg, (udp_ip, udp_port))
 
 def udp_to_ser():                                                       # UDP to serial thread
-	try:
-		udp_data, udp_client = udp_socket.recvfrom(1024)            	# receive data on UDP
-	except socket.timeout:
-		logging.error("Read timeout on socket")
-	except:
-            pass
-	else:
-		ser_write(udp_data)                                         	# write data to serial
+    try:
+        udp_data, udp_client = udp_socket.recvfrom(512)            	# receive data on UDP
+        #print "data: ", udp_data
+    except socket.timeout:
+	logging.error("Read timeout on socket")
+    except:
+        pass
+    else:
+	ser_write(udp_data)                                         	# write data to serial
 			
-def ser_to_udp():														# Serial to UDP thread
-	try:
-		ser_msg = py_serial.readline().strip()                          # Remove newline character '\n'
-	except serial.SerialTimeoutException as e:
-		logging.error("Read timeout on serial port")
-	except serial.SerialException as e:
-		logging.error("Read exception on serial port")
-	else:
-		udp_write(ser_msg)                                              # write to UDP
+def ser_to_udp():                                                       # Serial to UDP thread
+    msg = bytearray()
+    msg.append(0)
+    payload_length = bytearray()
+    payload_length.append(0)
+    try:
+        msg[0] = py_serial.read(1)
+        if msg[0] == 254:
+            payload_length[0] = py_serial.read()
+            msg.append(payload_length[0])
+            payload = py_serial.read(payload_length[0] + 6)
+            for x in payload:
+                msg.append(x)
+    except serial.SerialTimeoutException as e:
+	logging.error("Read timeout on serial port")
+    except serial.SerialException as e:
+	logging.error("Read exception on serial port")
+    else:
+        udp_write(msg)                                                  # write to UDP
 
 def main():
     global connection
     while connection is False:
         time.sleep(1)
-        udp_socket.sendto('hello', (udp_ip, udp_port))
+        udp_write('hello')
         try:
             udp_data, udp_client = udp_socket.recvfrom(1024)
             if udp_data is not None:
                 connection = True
+                print "GCS connection from: ", udp_client
         except:
             pass
         else:
@@ -107,7 +119,7 @@ py_serial = open_port(serial_port, baud_rate)
 if py_serial is None:
     print "Could not open serial port. Script exit."
     sys.exit()
-print "Waiting on port: ", udp_port
+print "Waiting for GCS on port: ", udp_port
 try:
     main()
 except KeyboardInterrupt:
